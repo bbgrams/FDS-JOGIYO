58937392f776e3611a426ba8f33ad30b9f74d93d
Hyunjoon Kim <joonkim0625@gmail.com>
2018-11-28 16:22:29 +0900

    레이아웃 페이지 생성

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="header">헤더</div>
+        <div className="footer">푸터</div>
+      </React.Fragment>
+    );
+  }
+}
