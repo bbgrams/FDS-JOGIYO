d709a8e6ad8ae9ae5ce2595950d9b155cead6bbe
JihyeHwang09 <raye9053@gmail.com>
2018-12-03 21:00:20 +0900

    FoodCategoryPage.js Link to 설정

--- src/pages/FoodCategoryPage.js
+++ src/pages/FoodCategoryPage.js
@@ -5,12 +5,40 @@ import { Link } from 'react-router-dom';
 
 export default class FoodCategoryPage extends Component {
   render() {
-    return (
-      <Layout>
-        <Link className="FoodCategory__link--all" to="/all">
+    return <Layout>
+        <Link className="FoodCategory__link--all" to="/restaurant">
           전체 보기
         </Link>
-      </Layout>
-    );
+        <Link className="FoodCategory__link--franchise" to="/restaurant/?">
+          프랜차이즈
+        </Link>
+        <Link className="FoodCategory__link--chicken" to="/restaurant/?category=chicken">
+          치킨
+        </Link>
+        <Link className="FoodCategory__link--all" to="/restaurant/?category=">
+          피자/양식
+        </Link>
+      <Link className="FoodCategory__link--all" to="/restaurant/?category=">
+          중국집
+        </Link>
+      <Link className="FoodCategory__link--all" to="/restaurant/?category=">
+          한식
+        </Link>
+      <Link className="FoodCategory__link--all" to="/restaurant/?category=">
+          일식/돈까스
+        </Link>
+      <Link className="FoodCategory__link--all" to="/restaurant/?category=">
+          족발/보쌈
+        </Link>
+        <Link className="FoodCategory__link--all" to="/all">
+          야식
+        </Link>
+        <Link className="FoodCategory__link--all" to="/all">
+          분식
+        </Link>
+        <Link className="FoodCategory__link--all" to="/all">
+          카페/디저트
+        </Link>
+      </Layout>;
   }
 }
