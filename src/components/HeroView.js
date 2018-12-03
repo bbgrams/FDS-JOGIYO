91c77ee0ef03dfe4f86bab00986cc06e7cf3f938
Hyunjoon Kim <joonkim0625@gmail.com>
2018-12-03 12:35:04 +0900

    HeroView.js 생성

---
+++ src/components/HeroView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class HeroView extends Component {
+  render() {
+    return (
+      <form className="Hero__form">
+        <fieldset>
+          <legend>위치 검색</legend>
+          <button className="Hero__gps-icon">GPS 아이콘</button>
+          <label htmlFor="location-search">위치 검색 창</label>
+          <input type="text" id="location-search" />
+          <button className="Hero__search-btn">검색</button>
+        </fieldset>
+      </form>
+    );
+  }
+}
