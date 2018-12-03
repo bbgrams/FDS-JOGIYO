d8cffc4422ff6deb2c68e909f50e35e7db0d85a1
Shim Soyoung <sovv12@gmail.com>
2018-12-03 19:56:07 +0900

    별점 소수점 표기 이슈 해결, 변경된 데이터명 수정

--- src/components/RestaurantListView.js
+++ src/components/RestaurantListView.js
@@ -14,16 +14,24 @@ export default class RestaurantListView extends Component {
             <img src={l.logoUrl} alt={l.name} className="RestaurantList__img" />
             <h1 className="RestaurantList__name">{l.name}</h1>
             <div className="RestaurantList__info-wrap">
-              <span className="RestaurantList__star">★</span>
-              <span className="RestaurantList__review">
-                리뷰 {l.review_count}
-              </span>
-              <span className="RestaurantList__owner">사장님댓글</span>
+              {l.reviewAvg <= 0 ? (
+                <span>첫 리뷰를 남겨주세요 !</span>
+              ) : (
+                <React.Fragment>
+                  <span className="RestaurantList__star">
+                    ★{parseFloat(l.reviewAvg).toFixed(1)}
+                  </span>
+                  <span className="RestaurantList__review">
+                    리뷰 {l.reviewCount}
+                  </span>
+                  <span className="RestaurantList__owner">사장님댓글</span>
+                </React.Fragment>
+              )}
             </div>
             <div className="RestaurantList__info-wrap">
               <span className="RestaurantList__payment">요기서 결제</span>
               <span className="RestaurantList__min-price">
-                {l.min_order_amount}원 이상 배달
+                {l.minOrderAmount}원 이상 배달
               </span>
             </div>
             <div>
