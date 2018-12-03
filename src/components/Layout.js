1b884252f6d673439d6a2b78201bfccbcdaf8aa1
Shim Soyoung <sovv12@gmail.com>
2018-12-03 11:34:28 +0900

    초기 설정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Layout extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="header">헤더</div>
-        <div className="footer">푸터</div>
-      </React.Fragment>
-    );
+    )
   }
 }
