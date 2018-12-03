9ef4c6e0d7faa5feafd7c7ea09397ed39a947801
Hyunjoon Kim <joonkim0625@gmail.com>
2018-12-03 16:47:14 +0900

    RestaurantList에 가데이터 반영 및 api.js 수정

--- src/components/RestaurantListView.js
+++ src/components/RestaurantListView.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 export default class RestaurantListView extends Component {
+  static defaultProps = {
+    list: [],
+  };
+
   render() {
-    return <div>ㅎㅎ</div>;
+    const { list } = this.props;
+    return (
+      <div>
+        {list.map(l => (
+          <div key={l.id}>
+            <div>{l.id}</div>
+            <div>{l.name}</div>
+            <img src={l.logo_url} alt={l.name} />
+            <div>{l.min_order_amount}</div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
--- src/pages/RestaurantListPage.js
+++ src/pages/RestaurantListPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import RestaurantList from '../containers/RestaurantList';
 
 export default class RestaurantListPage extends Component {
   render() {
     return (
-      <div>
-        <h1>음식점 목록</h1>
-      </div>
+      <Layout>
+        <RestaurantList />
+      </Layout>
     );
   }
 }
