22d4569dfb11200efa8350361ef027c44f8d292a
Hyunjoon Kim <joonkim0625@gmail.com>
2018-12-03 12:35:12 +0900

    Hero.js 생성

---
+++ src/containers/Hero.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import HeroView from '../components/HeroView';
+
+export default class Hero extends Component {
+  render() {
+    return <HeroView />;
+  }
+}
