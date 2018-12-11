3bc2bc0da409217eb74adfc74dbb198ee874f236
Hyunjoon Kim <joonkim0625@gmail.com>
2018-12-11 16:33:34 +0900

    StoreInfoView.js 작성 시작

--- src/components/StoreInfoView.js
+++ src/components/StoreInfoView.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 
 export default class StoreInfoView extends Component {
+  static defaultProps = {
+    info: [
+      // "begin": "11:30:00",
+      // "end": "20:00:00",
+      // "companyName": "씨제이푸드빌 (주)",
+      // "companyNumber": "312-81-42519",
+      // "countryOrigin": "원산지 더미 ㅡㅠㅡ",
+      // "introductionText": ""
+    ],
+  };
+
   render() {
-    return <div>상점 정보</div>;
+    const { info } = this.props;
+    return <div className="StoreInfo">업체 정보</div>;
   }
 }
