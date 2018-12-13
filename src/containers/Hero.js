8d0c8e834608a44aa5031e15aa86fc5a26e8b478
JihyeHwang09 <raye9053@gmail.com>
2018-12-13 17:38:07 +0900

    geolocation에서 위도, 경도값 받아와서 상태에 저장하기

--- src/containers/Hero.js
+++ src/containers/Hero.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react';
 import HeroView from '../components/HeroView';
 
 export default class Hero extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locationX: 0,
+      locationY: 0,
+    };
+  }
+
+  async componentDidMount() {
+    const handleSetState = this.handleSetState;
+    if (navigator.geolocation) {
+      // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          handleSetState(position.coords.latitude, position.coords.longitude);
+        },
+        function(error) {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity,
+        }
+      );
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  }
+
+  handleSetState = (latitude, longitude) => {
+    this.setState({ locationX: latitude, locationY: longitude });
+  };
+
   render() {
     return <HeroView />;
   }
