127292778cd966aeba040c79fa88c687340e075b
Shim Soyoung <sovv12@gmail.com>
2018-12-13 20:12:41 +0900

    위치 가져오는  context생성

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import CartPage from './pages/CartPage';
 import PayPage from './pages/PayPage';
-
+import { KakaoApiProvider } from './contexts/kakaoApiContext';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <Route exact path="/" component={FoodCategoryPage} />
-          <Route path="/category/:id" component={StoreListPage} />
-          <Route path="/store/:id" component={StoreDetailPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/pay" component={PayPage} />
-          <Route path="/cart" component={CartPage} />
-        </React.Fragment>
-      </BrowserRouter>
+      <KakaoApiProvider>
+        {' '}
+        <BrowserRouter>
+          <React.Fragment>
+            <Route exact path="/" component={FoodCategoryPage} />
+            <Route path="/category/:id" component={StoreListPage} />
+            <Route path="/store/:id" component={StoreDetailPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/pay" component={PayPage} />
+            <Route path="/cart" component={CartPage} />
+          </React.Fragment>
+        </BrowserRouter>
+      </KakaoApiProvider>
     );
   }
 }
