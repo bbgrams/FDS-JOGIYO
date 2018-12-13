674edf24df4352d37e618c264f07011837236dff
JihyeHwang09 <raye9053@gmail.com>
2018-12-13 18:15:47 +0900

    kakao api 이용해서  위도,경도를 텍스트로 변환해서 받음

--- src/components/HeroView.js
+++ src/components/HeroView.js
@@ -3,11 +3,14 @@ import './HeroView.scss';
 
 export default class HeroView extends Component {
   render() {
+    const { findMyAddress } = this.props;
     return (
       <form className="Hero">
         <fieldset>
           <legend className="readable-hidden">위치 검색</legend>
-          <button className="Hero__gps-icon">현재위치 찾기</button>
+          <button onClick={e => findMyAddress(e)} className="Hero__gps-icon">
+            현재위치 찾기
+          </button>
           <label className="readable-hidden" htmlFor="location-search">
             위치 검색
           </label>
--- src/containers/Hero.js
+++ src/containers/Hero.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import HeroView from '../components/HeroView';
+import axios from 'axios';
+
+const api = axios.create({
+  headers: { Authorization: 'KakaoAK 1a72ca0688a79816a4a07debdf9bc661' },
+});
+// https://dapi.kakao.com//v2/local/geo/coord2address.json
 
 export default class Hero extends Component {
   constructor(props) {
@@ -17,7 +23,7 @@ export default class Hero extends Component {
       // GPS를 지원하면
       navigator.geolocation.getCurrentPosition(
         function(position) {
-          handleSetState(position.coords.latitude, position.coords.longitude);
+          handleSetState(position.coords.longitude, position.coords.latitude);
         },
         function(error) {
           console.error(error);
@@ -33,11 +39,24 @@ export default class Hero extends Component {
     }
   }
 
-  handleSetState = (latitude, longitude) => {
-    this.setState({ locationX: latitude, locationY: longitude });
+  handleSetState = (longitude, latitude) => {
+    this.setState({ locationX: longitude, locationY: latitude });
   };
-
+  handleGpsClick = async e => {
+    e.preventDefault();
+    console.log(this.state.locationX, this.state.locationY);
+    const res = await api.get(
+      'https://dapi.kakao.com//v2/local/geo/coord2address.json',
+      {
+        params: {
+          x: this.state.locationX,
+          y: this.state.locationY,
+        },
+      }
+    );
+    console.log(res);
+  };
   render() {
-    return <HeroView />;
+    return <HeroView findMyAddress={this.handleGpsClick} />;
   }
 }
