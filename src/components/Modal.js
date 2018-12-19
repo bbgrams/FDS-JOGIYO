fb3e462271cfe693d6d2fadc42d0ee24686d3a88
Shim Soyoung <sovv12@gmail.com>
2018-12-19 14:52:05 +0900

    modal 스타일링

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -77,51 +77,51 @@ export default class Modal extends Component {
       <div className={showHideClassName}>
         {/* <div className="show"> */}
         <div className="Modal__main">
-          <div className="Madal__order">
-            <h1 className="Madal__order__title">계란짐{name}</h1>
+          <div className="Modal__order">
+            <h1 className="Modal__order__title">{name}</h1>
             <div
-              className="Madal__order__img"
+              className="Modal__order__img"
               style={{ backgroundImage: 'url(' + image + ')' }}
             >
               {name}
             </div>
-            <div className="Madal__order__price">
-              <span className="Madal__order__price__title">가격</span>
-              <span className="Madal__order__price__num">{price}원</span>
+            <div className="Modal__order__price">
+              <span className="Modal__order__price__title">가격</span>
+              <span className="Modal__order__price__num">{price}원</span>
             </div>
 
-            <div className="Madal__order__quantity">
-              <span className="Madal__order__quantity__title">수량</span>
-              <div className="Madal__order__quantity__btn">
+            <div className="Modal__order__quantity">
+              <span className="Modal__order__quantity__title">수량</span>
+              <div className="Modal__order__quantity__btn">
                 <button
                   onClick={() => this.handleQuantityMinus()}
-                  className="Madal__order__quantity__btn__minus"
+                  className="Modal__order__quantity__btn__minus"
                 >
-                  <FontAwesomeIcon icon={faMinus} />
+                  수량 감소
                 </button>
-                <span className="Madal__order__quantity__btn__num">
+                <strong className="Modal__order__quantity__btn__num">
                   {quantity}
-                </span>
+                </strong>
                 <button
                   onClick={() => this.handleQuantityPlus()}
-                  className="Madal__order__quantity__btn__plus"
+                  className="Modal__order__quantity__btn__plus"
                 >
-                  <FontAwesomeIcon icon={faPlus} />
+                  수량 추가
                 </button>
               </div>
             </div>
-            <div className="Madal__order__total_price">
-              <span className="Madal__order__total_price__title">
+            <div className="Modal__order__total_price">
+              <span className="Modal__order__total_price__title">
                 총 주문금액
               </span>
-              <span className="Madal__order__total_price__num">
+              <span className="Modal__order__total_price__num">
                 {totalPrice.toLocaleString()}원
               </span>
             </div>
-            <div className="Madal__order__min">
+            <div className="Modal__order__min">
               최소주문금액: {minAmount.toLocaleString()}원
             </div>
-            <div className="Madal__order__btn">
+            <div className="Modal__order__btn">
               <button
                 onClick={() =>
                   this.props.addToCart(
@@ -142,10 +142,9 @@ export default class Modal extends Component {
           </div>
           <button
             onClick={() => this.handleModalClose()}
-            className="Madal__order__close"
+            className="Modal__order__close"
           >
-            <FontAwesomeIcon icon={faTimes} />
-            닫기
+            창닫기
           </button>
         </div>
       </div>
