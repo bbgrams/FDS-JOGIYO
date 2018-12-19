ff81da36de1e71464db5c00fcca90716a721f8e0
Shim Soyoung <sovv12@gmail.com>
2018-12-19 12:24:04 +0900

    메인 카테고리 번호 수정

--- src/pages/FoodCategoryPage.js
+++ src/pages/FoodCategoryPage.js
@@ -13,37 +13,37 @@ export default class FoodCategoryPage extends Component {
             <Link className="FoodCategory__link--all" to="/category">
               전체 보기
             </Link>
-            <Link className="FoodCategory__link--alone" to="/category/10">
+            <Link className="FoodCategory__link--alone" to="/category/8">
               1인분 주문
             </Link>
             <Link className="FoodCategory__link--franchise" to="/category/2">
               프랜차이즈
             </Link>
-            <Link className="FoodCategory__link--chicken" to="/category/3">
+            <Link className="FoodCategory__link--chicken" to="/category/7">
               치킨
             </Link>
             <Link className="FoodCategory__link--pizza" to="/category/1">
               피자/양식
             </Link>
-            <Link className="FoodCategory__link--chinese" to="/category/1">
-              중국집
+            <Link className="FoodCategory__link--chinese" to="/category/11">
+              중식
             </Link>
             <Link className="FoodCategory__link--korean" to="/category/6">
               한식
             </Link>
-            <Link className="FoodCategory__link--japanese" to="/category/11">
+            <Link className="FoodCategory__link--japanese" to="/category/12">
               일식/돈까스
             </Link>
-            <Link className="FoodCategory__link--pig" to="/category/7">
+            <Link className="FoodCategory__link--pig" to="/category/4">
               족발/보쌈
             </Link>
-            <Link className="FoodCategory__link--night" to="/category/9">
+            <Link className="FoodCategory__link--night" to="/category/10">
               야식
             </Link>
-            <Link className="FoodCategory__link--snack" to="/category/8">
+            <Link className="FoodCategory__link--snack" to="/category/9">
               분식
             </Link>
-            <Link className="FoodCategory__link--cafe" to="/category/4">
+            <Link className="FoodCategory__link--cafe" to="/category/3">
               카페/디저트
             </Link>
           </div>
